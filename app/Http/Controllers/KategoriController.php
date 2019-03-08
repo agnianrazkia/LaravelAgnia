e6ce65950aaec54f13f991b3c40aca8e97af6291
agnianrazkia <agnianrazkia@gmail.com>
2019-03-08 11:50:04 +0700

    Episode 27

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -48,7 +48,14 @@ class KategoriController extends Controller
             'kategori'=>'required|between:3,100|unique:kategori,nama_kategori,'.$req->id,
         ])->validate();
 
-        
+        $result = Kategori::where('id',$req->id)
+                    ->update([
+                        'nama_kategori'=>$req->kategori,
+                    ]);
+        if ( $result ) {
+            return redirect()->route('admin.kategori')->with('result','update');
+        } else {
+            return back()->with('result','fail');
         }
     }
 }
